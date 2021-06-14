bc5786b5123802d95ddb8df24f1cdf29378fbf05
Swistusmen <michal76aw@gmail.com>
2021-06-14 23:45:47 +0200

    screenshot

--- Viewer/draw_function.py
+++ Viewer/draw_function.py
@@ -1,11 +1,20 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+
 from mpl_toolkits import mplot3d
 from mpl_toolkits.mplot3d import Axes3D
-
+M_PI=3.14159
 def fun(x,y):
-    return pow(x * x + y - 11, 2) + pow(x + y * y - 7, 2)
+    #return pow(x * x + y - 11, 2) + pow(x + y * y - 7, 2) Himmelblau
+    '''return (
+        np.sin(3 * np.pi * x) ** 2
+        + ((x - 1) ** 2) * (1 + np.sin(3 * np.pi * y) ** 2)
+        + ((y - 1) ** 2) * (1 + np.sin(2 * np.pi * y) ** 2)
+    )'''
+    return (
+        20 + x ** 2 - 10 * np.cos(2 * np.pi * x) + y ** 2 - 10 * np.cos(2 * np.pi * y)
+    )
 
 x=np.linspace(-5.12, 5.12,30)
 y=np.linspace(-5.12,5.12,30)
